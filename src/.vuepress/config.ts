f64bee91c4c174c494ec08a19e567105851a87c4
Zatursure <im_zatursure@163.com>
2026-01-14 22:37:28 +0800

    third commit

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -77,14 +77,14 @@ export default defineUserConfig({
      * Shiki 代码高亮
      * @see https://theme-plume.vuejs.press/config/plugins/code-highlight/
      */
-    // codeHighlighter: {
+    codeHighlighter: {
     //   twoslash: true, // 启用 twoslash
     //   whitespace: true, // 启用 空格/Tab 高亮
-    //   lineNumbers: true, // 启用行号
-    // },
+      lineNumbers: true, // 启用行号
+    },
 
     /* 文章字数统计、阅读时间，设置为 false 则禁用 */
-    // readingTime: true,
+    readingTime: true,
 
     /**
      * markdown
@@ -92,16 +92,16 @@ export default defineUserConfig({
      */
     markdown: {
     //   abbr: true,         // 启用 abbr 语法  *[label]: content
-    //   annotation: true,   // 启用 annotation 语法  [+label]: content
-    //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
+      annotation: true,   // 启用 annotation 语法  [+label]: content
+      pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
     //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
       plot: true,         // 启用隐秘文本语法 !!xxxx!!
       bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
-    //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
-    //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
+      youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
+      artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
     //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
       icon: { provider: 'fontawesome', assets: ['https://jsdmirrors.aino.cyou/npm/@fortawesome/fontawesome-free@7.1.0/js/all.min.js'] },
-    //   table: true,        // 启用表格增强容器语法 ::: table
+      table: true,        // 启用表格增强容器语法 ::: table
     //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
     //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
     //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
