62f6f6a36af53bdd97b30890f8b344baa96c0702
Zatursure <im_zatursure@163.com>
2026-01-17 14:03:32 +0800

    启用折叠容器和代码演示容器功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -108,13 +108,13 @@ export default defineUserConfig({
     //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
     //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
     //   demo: true,         // 启用 demo 容器  ::: demo
-    //   collapse: true,     // 启用折叠容器  ::: collapse
-    //   repl: {             // 启用 代码演示容器
-    //     go: true,         // ::: go-repl
-    //     rust: true,       // ::: rust-repl
-    //     kotlin: true,     // ::: kotlin-repl
-    //     python: true,     // ::: python-repl
-    //   },
+      collapse: true,     // 启用折叠容器  ::: collapse
+      repl: {             // 启用 代码演示容器
+        go: true,         // ::: go-repl
+        rust: true,       // ::: rust-repl
+        kotlin: true,     // ::: kotlin-repl
+        python: true,     // ::: python-repl
+      },
     //   math: {             // 启用数学公式
     //     type: 'katex',
     //   },
